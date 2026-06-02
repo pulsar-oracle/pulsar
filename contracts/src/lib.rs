d0fda796099153af9fb3240282073b7f32b4c0cd
sanmipaul <sanmipaul1@gmail.com>
2026-06-02 23:10:13 +0100

    style(contracts): apply cargo fmt

--- contracts/src/lib.rs
+++ contracts/src/lib.rs
@@ -20,10 +20,7 @@ impl PulsarOracle {
     /// Submit a data point for a feed (feeder nodes call this)
     pub fn submit(env: Env, feeder: Address, feed_id: Symbol, value: i128) {
         feeder.require_auth();
-        assert!(
-            feeds::is_registered(&env, &feed_id),
-            "feed not registered"
-        );
+        assert!(feeds::is_registered(&env, &feed_id), "feed not registered");
         let submission = Submission {
             feeder,
             value,
